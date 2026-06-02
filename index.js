61d6953ac72152e47bbf43b9192a727033c25950
PAVANA809 <pavanhapavan@gmail.com>
2026-06-02 20:29:52 +0530

    unknown commit

--- index.js
+++ index.js
@@ -570,7 +570,7 @@ app.get('/search/:query', async (req, res) => {
     });
 
     const port = process.env.PORT || 3000;
-    app.listen(port, () => {
+    app.listen(port, '0.0.0.0', () => {
         console.log(`Server is running on port:${port}`);
     });
 })();
